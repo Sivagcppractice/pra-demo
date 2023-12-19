2807fa00ae1895dc8dd3ff257357b4c2a214b074
“Sivagcppractice” <gcppractice560@gmail.com>
2023-12-19 15:27:16 +0530

    changed num1 values in siva branch

--- addition.java
+++ addition.java
@@ -3,21 +3,11 @@ class addition
         public static void main(string args[])
         {
                 int num1,num2,res;
-                num1=20;
+                num1=2005;
                 num2=45;
                 res=num1+num2;
                 system.out.println("The sum is "+res);
-//this is a comment
+                system.out.println("thankyou");
+//This a comment
+        }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
