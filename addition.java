25db8371a3e3b3d6287115714fa48199c445e8f0
“Sivagcppractice” <gcppractice560@gmail.com>
2023-12-19 15:24:31 +0530

    modified num1 value

--- addition.java
+++ addition.java
@@ -3,21 +3,10 @@ class addition
         public static void main(string args[])
         {
                 int num1,num2,res;
-                num1=20;
+                num1=2005;
                 num2=45;
                 res=num1+num2;
                 system.out.println("The sum is "+res);
-//this is a comment
+//This a comment
 }
 }
-
-
-
-
-
-
-
-
-
-
-
